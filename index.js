9166bf2efd0fe3d250b27cf3738f5c98a74643de
010 Pixel <rkgandhi@mibtt-rkgandhi1.users.sphinet.com.sg>
2017-06-23 18:11:40 +0800

    Added node.

---
+++ index.js
@@ -0,0 +1,15 @@
+var http = require('http'),
+    fs = require('fs');
+
+var server_port = process.env.PORT || 9000;
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(server_port);
+});
